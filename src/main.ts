9a015d984585a23f708069421758a2cabfd24f27
Andres Vega <aevega1991@gmail.com>
2025-06-27 19:24:57 -0300

    fix: :bug: changed line endings and scaffolded the service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger();
+  const PORT = process.env.PORT ?? 4000;
+  await app.listen(PORT);
+  logger.log(`Service started at port ${PORT}`);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Bootstrap failed', err);
+  process.exit(1);
+});
